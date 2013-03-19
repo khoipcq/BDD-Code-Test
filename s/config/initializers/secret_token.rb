d429d944736c953e3bf672105f0b7ff8693a5e78
nhannm <nhannm@elarion.com>
2013-03-19 09:10:16 +0700

    A

---
+++ s/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S::Application.config.secret_token = 'b618fed6936a60fc276574bd29e7e77bd9c5bebaa1b7b3b578c182c43e670bcbc16ec0409f69e9b3bbd1cfdfd91ff774941ded7005baea8ed76e0d30a37e91be'
